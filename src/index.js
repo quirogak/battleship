62405aeeeb40305725810cfa9cda75c178376d52
quirogak <quirogakill@gmail.com>
2023-03-21 13:35:06 -0500

    Ship factory tested successfully.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-function sum(a, b) {
-  return a + b;
-}
 
-console.log(sum);
-export { sum };
+const mainObjects = (() => {
+
+  const Ship = (lengthNumber, hitsNumber) => {
+
+    const checkSunk = (length, hits) => {
+
+      if (length === hits) return true
+      return false
+    }
+
+    let shipHits = hitsNumber
+
+    const shipLength = lengthNumber
+
+    const isSunk = () => checkSunk(shipLength, shipHits)
+
+    const hit = () => {
+      shipHits++
+      return shipHits
+    }
+
+
+
+    return { shipLength, shipHits, isSunk, hit }
+
+  }
+
+  return { Ship }
+
+})();
+
+
+export { mainObjects }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,27 @@
-import { sum } from "./index";
+import { mainObjects } from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("can check if a ship is not sunk", () => {
+  expect(mainObjects.Ship(4, 0).isSunk()).toBe(false);
 });
+
+test("can check if a ship is sunk", () => {
+  expect(mainObjects.Ship(3, 3).isSunk()).toBe(true);
+});
+
+test("can increase the number of hits", () => {
+  expect(mainObjects.Ship(4, 0).hit()).toBe(1);
+});
+
+describe('hit-sunk test', () => {
+
+  const testShip = mainObjects.Ship(4, 3)
+
+  beforeEach(() => testShip.hit()); // makes the length number the same as the hit number
+
+  test('can increase the number of hits and make a ship sunk', () => {
+    expect(testShip.isSunk()).toBe(true);
+  });
+
+});
+
+
