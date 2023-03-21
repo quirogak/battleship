c19e6a243928838f3fe7fddcd8f4bd36c0815d6c
quirogak <quirogakill@gmail.com>
2023-03-21 15:23:58 -0500

    Gameboard new board tests passed. can create a 10x10 game board. the game board has X both and Y coordinates correct

--- src/index.js
+++ src/index.js
@@ -1,34 +1,47 @@
-
 const mainObjects = (() => {
-
   const Ship = (lengthNumber, hitsNumber) => {
-
     const checkSunk = (length, hits) => {
+      if (length === hits) return true;
+      return false;
+    };
 
-      if (length === hits) return true
-      return false
-    }
+    let shipHits = hitsNumber;
 
-    let shipHits = hitsNumber
+    const shipLength = lengthNumber;
 
-    const shipLength = lengthNumber
-
-    const isSunk = () => checkSunk(shipLength, shipHits)
+    const isSunk = () => checkSunk(shipLength, shipHits);
 
     const hit = () => {
-      shipHits++
-      return shipHits
-    }
+      shipHits++;
+      return shipHits;
+    };
 
+    return { isSunk, hit };
+  };
 
+  const Gameboard = (size) => {
+    const newBoard = () => {
+      const board = [];
 
-    return { shipLength, shipHits, isSunk, hit }
+      const recursive = (yPos) => {
+        if (yPos === size) return;
 
-  }
+        for (let i = 0; i < size; i++) {
+          board.push([yPos, i]);
+        }
 
-  return { Ship }
+        recursive(yPos + 1);
+      };
 
+      recursive(0);
+
+      return board;
+    };
+
+    return { newBoard };
+  };
+
+  return { Ship, Gameboard };
 })();
 
-
-export { mainObjects }
+export { mainObjects };
--- src/index.test.js
+++ src/index.test.js
@@ -1,27 +1,36 @@
 import { mainObjects } from "./index";
 
-test("can check if a ship is not sunk", () => {
-  expect(mainObjects.Ship(4, 0).isSunk()).toBe(false);
+describe("ship test", () => {
+  test("can check if a ship is not sunk", () => {
+    expect(mainObjects.Ship(4, 0).isSunk()).toBe(false);
+  });
+
+  test("can check if a ship is sunk", () => {
+    expect(mainObjects.Ship(3, 3).isSunk()).toBe(true);
+  });
+
+  test("can increase the number of hits", () => {
+    expect(mainObjects.Ship(4, 0).hit()).toBe(1);
+  });
 });
 
-test("can check if a ship is sunk", () => {
-  expect(mainObjects.Ship(3, 3).isSunk()).toBe(true);
-});
-
-test("can increase the number of hits", () => {
-  expect(mainObjects.Ship(4, 0).hit()).toBe(1);
-});
-
-describe('hit-sunk test', () => {
-
-  const testShip = mainObjects.Ship(4, 3)
+describe("hit-sunk test", () => {
+  const testShip = mainObjects.Ship(4, 3);
 
   beforeEach(() => testShip.hit()); // makes the length number the same as the hit number
 
-  test('can increase the number of hits and make a ship sunk', () => {
+  test("can increase the number of hits and make a ship sunk", () => {
     expect(testShip.isSunk()).toBe(true);
   });
-
 });
 
+describe("gameboard test", () => {
+  test("can create a 10x10 game board", () => {
+    expect(mainObjects.Gameboard(10).newBoard().length).toBe(100);
+  });
 
+  test("the game board has X both and Y coordinates correct", () => {
+    expect(mainObjects.Gameboard(10).newBoard()[10]).toEqual([1, 0]);
+    expect(mainObjects.Gameboard(10).newBoard()[22]).toEqual([2, 2]);
+  });
+});
